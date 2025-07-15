da8fdf43584be7e8d9c22f0870c3ca76bcc0a7fd
Hockenbimben <juansavila@gmail.com>
2025-07-15 13:51:45 -0500

    added the freaking triangle

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -12,12 +13,15 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    print(f"Player type: {type(player)}")
+    print(f"Player draw method: {player.draw}")
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black") 
+        player.draw(screen)
         pygame.display.flip()  
         dt = clock.tick(60) / 1000
 
---
+++ player.py
@@ -0,0 +1,24 @@
+import pygame
+from circleshape import CircleShape
+from constants import *
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+        print(f"Player created at: ({x}, {y})")
+        print(f"Player radius: {PLAYER_RADIUS}")
+
+    # in the player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        triangle_points = self.triangle()
+        pygame.draw.polygon(screen, "white", triangle_points, 2)
+
